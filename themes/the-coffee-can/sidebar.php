566c234f9017b1ce062b20f43c43f7fff9b70481
PengjieJCU <jie.peng@my.jcu.edu.au>
2019-05-23 10:24:06 +1000

    commit sidebar

--- themes/the-coffee-can/sidebar.php
+++ themes/the-coffee-can/sidebar.php
@@ -12,6 +12,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+<div id="secondary" class="widget-area" style="width:20%;">
+
+	<?php dynamic_sidebar( 'sidebar-1' ); ?><!-- #secondary -->
+</div>
